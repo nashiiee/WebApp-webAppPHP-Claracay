3313cc2b4641d842a7c0e0070285200fff7b3382
Nash <149981049+nashiiee@users.noreply.github.com>
2024-09-24 15:11:00 +0800

    Week 3
    yeah

--- nashies-web-app-2413/index.php
+++ nashies-web-app-2413/index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php
+    function displayOrderSummary() {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             echo "<h2>Order Summary</h2>";
 
@@ -32,6 +33,8 @@
 
             $coffee_type = $_POST["coffee"];
             $size = $_POST["size"];
+            $instructions = $_POST["instructions"];
+            $name = $_POST["name"];
 
             if(isset($_POST["extras"])) {
                 $extras = $_POST["extras"];
@@ -39,27 +42,78 @@
                 $extras = [];
             }
 
-            $total_price = $coffee_prices[$coffee_type] + $size_prices[$size];
+            // $total_price = $coffee_prices[$coffee_type] + $size_prices[$size];
+
+            // foreach($extras as $extra) {
+            //     $total_price = $total_price + $extras_prices[$extra];
+            // }
+
+            $total_price = calculatePrice($coffee_prices, $size_prices, $extras_prices, $coffee_type, $size, $extras);
+
+            echo $name;
+            echo "<br>";
+            echo $instructions;
+            echo "<br>";
+            echo $total_price;
+
+            $receiptContent = generateReceipt($name, $coffee_type, $size, $total_price, $instructions);
+            saveReceipt($receiptContent);
+
+            displayJoke($coffee_type, $total_price);
+
+            
+        }
+    }
+      
+
+    function calculatePrice($coffee_prices, $size_prices, $extras_prices, $coffee_type, $size, $extras) {
+        $total_price = $coffee_prices[$coffee_type] + $size_prices[$size];
 
             foreach($extras as $extra) {
                 $total_price = $total_price + $extras_prices[$extra];
             }
 
-            echo $total_price;
-            echo "<br/>";
+            return $total_price;
+    }
 
-            if ($coffee_type !== "espresso") {
-                echo "<br>";
-                echo "Hey, ", htmlspecialchars($_POST["name"]) . "!";
-                echo "<p>Here's a joke for you: Why did the coffee file a police report? It got mugged!</p>";
-            } 
+    function generateReceipt($name, $coffee_type, $size, $total_price, $instructions) {
+        $receiptContent = "Order Summary\n";
+        $receiptContent .= "---------------------\n";
 
-            if ($total_price > 450 && $total_price < 700) {
-                echo "<p>Password for the CR: coffee 123</p>";
-            } elseif ($total_price >= 900) {
-                echo "<p>Password for Wi-Fi: mocha456";
-            }
+        $receiptContent .= "Name" . $name . "\n";
+        $receiptContent .= "Coffee Type: " . $coffee_type . "\n";
+        $receiptContent .= "Size: " . $size . "\n";
+        $receiptContent .= "Total Price: " . $total_price . "\n";
+        $receiptContent .= "instructions: " . $instructions . "\n";
+        $receiptContent .= "Thank you for ordering!";
+
+        return $receiptContent;
+    }
+
+    function saveReceipt($receiptContent) {
+        $file = fopen("Order Summary.txt", "w") or die("Unable to Open text file");
+        fwrite($file, $receiptContent);
+
+        fclose($file);
+        echo "<br>";
+        echo "Receipt saved.";
+    }
+
+    function displayJoke($coffee_type, $total_price) {
+        if ($coffee_type !== "espresso") {
+            echo "<br>";
+            echo "Hey, ", htmlspecialchars($_POST["name"]) . "!";
+            echo "<p>Here's a joke for you: Why did the coffee file a police report? It got mugged!</p>";
+        } 
+
+        if ($total_price > 450 && $total_price < 700) {
+            echo "<p>Password for the CR: coffee 123</p>";
+        } elseif ($total_price >= 900) {
+            echo "<p>Password for Wi-Fi: mocha456";
         }
+    }
+
+    displayOrderSummary();
     ?>
 </body>
 </html>
